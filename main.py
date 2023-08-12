aeebce24eb9b728cee863378cb39b345ad1536ad
Ozan Yetgin <ozanyetgin1@gmail.com>
2023-08-12 20:03:38 +0300

    main.py oluşturuldu

---
+++ main.py
@@ -0,0 +1,5 @@
+import random
+
+gerceklerim = ["Hobim, yani okçuluk diyebilirim","Amatör rap kaydı yapıyorum","Okulumun robotik takımında tasarımcıyım","Muhtemelen pizza"]
+print(gerceklerim)
+print(random.choice(gerceklerim))
